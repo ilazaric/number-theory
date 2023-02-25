d9af8012efa2e936f348e8dc61bb96b8245a6732
Ivan Lazaric <ivan.lazaric1@gmail.com>
2023-02-25 08:09:40 +0000

    add perf test

---
+++ perftest/test.cpp
@@ -0,0 +1,19 @@
+#include <ivl/bignum.hpp>
+
+#include <iostream>
+
+int main()
+{
+  using Integer = ivl::nt::Bignum<std::uint32_t, 10>;
+
+  Integer a{ 123 };
+  Integer b{ 10219 };
+  Integer c{ 1092 };
+
+  for (std::uint32_t i = 0; i < 5000; ++i) { c = c * a + b; }
+
+  c %= Integer{ 1000000007 };
+  std::cout << c.template get<std::int64_t>() << std::endl;
+
+  return 0;
+}
